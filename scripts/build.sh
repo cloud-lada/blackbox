40d0b66e634af23b473e59fc7e62a9b02dda9e12
David Bond <davidsbond93@gmail.com>
2022-06-10 23:02:56 +0100

    Publish code as a binary to GPR
    This commit is the first attempt to update the CI workflow to use github
    packages for the distribution of the CLI. In theory, we can then just
    do `yarn global install` etc and run `blackbox` right off the bat.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,6 @@ rm -f blackbox.js
 rm -rf build
 
 esbuild src/main.ts --bundle --outfile=blackbox.js --platform=node --minify --analyze
+chmod +x blackbox.js
 mkdir -p build
 cp node_modules/rpio/build/Release/rpio.node build/rpio.node
